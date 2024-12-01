6829ad1ff72515a7fd30540df28f93a42d7f9a4c
theaminnouri <theaminnouri@gmail.com>
2024-12-01 14:54:43 +0330

    Solve missing-number-268
    https://leetcode.com/problems/missing-number/description/

---
+++ src/main/kotlin/problem/MissingNumber268.kt
@@ -0,0 +1,26 @@
+package org.example.problem
+
+object MissingNumber268 {
+    fun execute() {
+
+    }
+
+    fun optimalMissingNumber(nums: IntArray): Int {
+        val sum = nums.sum()
+        val n = nums.size
+        val expectedSum = (n * (n + 1)) / 2
+        return expectedSum - sum
+    }
+
+    fun missingNumber(nums: IntArray): Int {
+        val sortedNums = nums.sorted()
+        var missing = -1
+        for (i in 0..sortedNums.size) {
+            if (i > sortedNums.lastIndex || i != sortedNums[i]) {
+                missing = i
+                break
+            }
+        }
+        return missing
+    }
+}
